b3a758b6c28cf2dc22f7bc5dd85db7f9cb14e5d0
Marcio Ivan <marciofilho96@yahoo.com.br>
2018-04-17 14:38:26 -0300

    Add .gitignore

--- tcp-server/client.c
+++ tcp-server/client.c
@@ -16,9 +16,9 @@
 
 #define PORT "3490" // the port client will be connecting to
 
-#define MAXDATASIZE 1500 // max number of bytes we can get at once
+#define MAXDATASIZE 100 // max number of bytes we can get at once
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 100
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa);
