5dce6d41a4e4cc32d5b4ee43e9bc1f95ebecca67
Thomas Jun Yamasaki <tjun.yamasaki@gmail.com>
2018-06-13 23:01:15 -0300

    First commit - Dummy functional

---
+++ rmi-project/Hello.java
@@ -0,0 +1,9 @@
+package teste1;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Hello extends Remote {
+    String sayHello() throws RemoteException;
+    String saudarMarcio(int n) throws RemoteException;
+}
---
+++ rmi-project/Server.java
@@ -0,0 +1,37 @@
+package teste1;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+        
+public class Server implements Hello {
+        
+    public Server() {}
+
+    public String sayHello() {
+        return "Hello, world!";
+    }
+    
+    public String saudarMarcio(int n) {    	
+    	return ("Hoje teremos que saudar o Marcio " + n + " vezes");
+    }
+        
+    public static void main(String args[]) {
+        
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
